5f6a93711992d6dc6cbcaa61ce0f65ffabe3d35d
sumityadav1509 <sumityadav2005@gmail.com>
2020-12-03 17:20:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+bmi= int(weight)/float(height)**2
+bmi_as_integer=int(bmi)
+print(bmi_as_integer)
+
+
+
+
+
+
+
+
