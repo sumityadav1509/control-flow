fdcf12af7b9355a8ca715f5634e1b389158ba73e
sumityadav1509 <sumityadav2005@gmail.com>
2020-12-07 19:26:01 +0000

    if-else statement and flow control tutorial completed

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-# If the bill was $150.00 , split between 5 people , with 12% tip . 
-# Each person should pay (150/5)*1.12 
-# Round the result to 2 Decimal places = 33.66 
+print("welcome to the rollercoaster ! ") 
+height = int(input("What is your height in cm ? "))
 
-print("Welcome to the tip calculator") 
-bill = float(input("What is the bill amount ? $"))
-tip = int(input("What percentage tip you want to give ? "))
-people = int(input("Number of people ? "))
-
-total_amount = bill+(bill*tip/100)
-each_person = total_amount/people
-final_amount= round(each_person,2) 
-print(f"Each person should pay: $ {final_amount}") 
+if height>120: 
+  print("You can ride the rollercaoster ! ")
+else:
+  print("Sorry , you have to grow taller before you ride ! ") 
