7eed0719eeb7c34a31e10c48bed43fa04377ce34
sumityadav1509 <sumityadav2005@gmail.com>
2020-12-17 16:31:39 +0000

    Used nested if statement for adding picture taking charges in the final bill.

--- main.py
+++ main.py
@@ -4,13 +4,24 @@ height = int(input("What is your height in cm ? "))
 if height>=120: 
   print("You can ride the rollercaoster ! ")
   age=int(input("Please enter your age !"))
+  bill = 0
   if age<12:
-    print("Please pay $5.")
+    bill=5
+    print("Child Ticket price is $5.")
  
   elif age<=18:
-    print("Please pay $7.")
+    bill=7
+    print("Youth Ticket price is $7.")
   else:
-   print("Please pay $12.")
+   bill=12
+   print("Adult Ticket price is $12.")
+  
+  wants_pic = input("Do you want to take a photo ? Y or N ")
+  if wants_pic=="Y": 
+    bill+=3 
+
+  print(f"Your final bill is ${bill}")
+
 else:
   print("Sorry , you have to grow taller before you ride !") 
 
