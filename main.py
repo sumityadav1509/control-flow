3a08da9011a2d0254ff5a6165a0174536dd7f286
sumityadav1509 <sumityadav2005@gmail.com>
2020-12-17 14:45:05 +0000

    Used nested if-else statement and elif to introduce a bunch of statements.

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 print("welcome to the rollercoaster ! ") 
 height = int(input("What is your height in cm ? "))
 
-if height>120: 
+if height>=120: 
   print("You can ride the rollercaoster ! ")
+  age=int(input("Please enter your age !"))
+  if age<12:
+    print("Please pay $5.")
+ 
+  elif age<=18:
+    print("Please pay $7.")
+  else:
+   print("Please pay $12.")
 else:
-  print("Sorry , you have to grow taller before you ride ! ") 
+  print("Sorry , you have to grow taller before you ride !") 
+
